a4576c37d1f49da7a6e7b8fac1ece5f45e97b823
kostyaget <progogyd@gmail.com>
2022-10-20 16:52:18 +0300

    upd

--- js/navigation/nav-mobile.js
+++ js/navigation/nav-mobile.js
@@ -1,12 +1,3 @@
-// function myFunction() {
-//         var x = document.getElementById("myLinks");
-//         if (x.style.display === "block") {
-//           x.style.display = "none";
-//         } else {
-//           x.style.display = "block";
-//         }
-//       }
-
 $(document).ready(function () {
   $('.header__burger').click(function () {
     $('.header__burger,.header__menu').toggleClass('active');
