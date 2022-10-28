7ade8dbf641a0d11931e6c6bc4d9831ab35e27ad
kostyaget <progogyd@gmail.com>
2022-10-28 14:09:12 +0300

    upg

---
+++ js/controls.js
@@ -0,0 +1,12 @@
+'use strict';
+const refs = {
+    controls: document.querySelector(' #tabs-1[data-controls]'),
+    panes: document.querySelector(' #tabs-1[data-panes]'),
+};
+
+refs.controls.addEventListener('click', event => {
+
+
+    if (event.target.nodeName !== 'A') {
+    }
+ });
