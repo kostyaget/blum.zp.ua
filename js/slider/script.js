b6fc5ae6b0c017d9193d3db505396629ed837cbf
kostyaget <progogyd@gmail.com>
2022-11-11 18:26:49 +0200

    new slider

---
+++ js/slider/script.js
@@ -0,0 +1,14 @@
+new Swiper('.image-slider', {
+    autoplay: {
+        delay: 2000,
+        stopOnLastSlide: false,
+        disableOnInteration: false
+    },
+    speed: 600,
+
+    effect: 'fade',
+    fadeEffect: {
+        crossFade: true
+    },
+});
+
